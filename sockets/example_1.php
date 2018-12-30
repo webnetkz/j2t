b2ac471ba5b6a599ad0b917aec0a2f5559d1bddf
Yaroslav <8gg001@gmail.com>
2018-12-30 20:56:08 +0600

    3
    3

--- sockets/example_1.php
+++ sockets/example_1.php
@@ -6,11 +6,13 @@ $headers = "GET /webnet HTTP/1.1\r\n" .
 	"Host: localhost\r\n" .
 	"\r\n";
  // Проверяем соединение
-if (socket_connect($socket, '127.0.0.1', 80)) {
+if (socket_connect($socket, '127.0.0.1', 80))
+{
 	 // Записываем сокет и заголовки
 	socket_write($socket, $headers);
 	 // Записываем ответ от сервера и выводим его
-	while ($data = socket_read($socket, 1024)) {
+	while ($data = socket_read($socket, 1024))
+	{
 		echo $data;
 	}
 }
