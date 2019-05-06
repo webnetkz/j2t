a72d943d24c52057dc783a7cb9a9425f5d514614
webnetkz <8gg001@gmail.com>
2019-05-06 22:04:23 +0600

    start new project

--- MainParsing/function.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$str = file_get_contents('http://theory.phphtml.net');
-echo "<pre>";
-var_dump($str);
-?>
--- cURL/exampleCURL_1.php
+++
@@ -1,6 +0,0 @@
-<?php
- // Инициализируем cURL 
-$curl = curl_init('localhost/webnet/');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($curl);
-echo $result;
