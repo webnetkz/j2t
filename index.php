60cfef7d1736d274671667774c7bf196b62cbdfb
webnetkz <8gg001@gmail.com>
2019-05-09 00:58:08 +0600

    version 0.001 Alpha

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Игрушки Hola</title>
+
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="rgb(255, 255, 255)">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="RetactorPhoto by WebNet">
+        <meta name="keywords" content="RedactorPhoto">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="/public/images/almatyholaLogo.png" type="image/png">
+        <link rel="stylesheet" href="/public/styles/style.css">
+        <link rel="stylesheet" href="/public/styles/mobileStyle.css">
+        <link rel="manifest" href="/manifest.json">
+        
+    </head>
+
+    <body>
+
+        <header>
+            <a href="https://almatyhola.ml">
+                <img src="public/images/almatyholaLogo.png" alt="toys hola" class="headerLogo">
+            </a>
+        </header>
+        
+        <div class="bgImg">
+            <img src="public/images/almatyholaLogo.png" alt="toys hola">
+        </div>
+
+
+
+        <script>
+             // Проверка на поддержку service worker
+            if('serviceWorker' in navigator) {
+                navigator.serviceWorker
+                    .register('/sw.js')
+                    .then(function() { console.log("Service Worker Registered"); });
+            }
+        </script>
+        <script src="/public/scripts/main.js"></script>
+    </body>
+</html>
