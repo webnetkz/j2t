8f5a92f375110dd161811f33f6e2f42702663ae3
webnetkz <dbqqbq@gmail.com>
2022-09-12 10:11:43 +0600

    update

---
+++ index.php
@@ -0,0 +1,306 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Comodo Games</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+	<link rel="stylesheet" href="vendors/animate-css/animate.css">
+	<link rel="stylesheet" href="vendors/flaticon/flaticon.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+	<?php require_once "./components/header.php"; ?>
+
+	<!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="home_left_img">
+							<img class="img-fluid" src="img/banner/home-left.png" alt="">
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="banner_content">
+							<h2>
+              MT5 GLOBAL
+							</h2>
+							<p>
+              Access from a single account to exchanges in the USA, Europe and Asia
+Stocks, bonds, commodity futures, currencies, including cryptocurrencies
+							</p>
+							<div class="d-flex align-items-center">
+								<a id="play-home-video" class="video-play-button" href="./j2t_index_en.mp4">
+									<span></span>
+								</a>
+								<div class="watch_video text-uppercase">
+									watch the video
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Start About Us Area =================-->
+	<section class="about_us_area section_gap_top">
+		<div class="container">
+			<div class="row about_content align-items-center">
+				<div class="col-lg-6">
+					<div class="section_content">
+						<h6>OUR ADVANTAGES</h6>
+						<h1>American access
+stock<br> market provided
+Just2Trade Inc. USA is a member of FINRA
+lime.com</h1>
+						<p><ul>
+              <li>MetaTrader 5 trading platform for all kinds of devices</li>
+              <li>Payment of dividends on all securities</li>
+              <li>24 hour service
+support</li>
+            </ul></p>
+						<a class="primary_btn" href="#">Learn More</a>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="about_us_image_box justify-content-center">
+						<img class="img-fluid w-100" src="img/about_img.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End About Us Area =================-->
+
+	<!--================First Upcoming Games Area =================-->
+	<section class="upcoming_games_area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="main_title">
+						<h2>A FULL RANGE OF
+</h2>
+						<h1>FINANCIAL INSTRUMENTS ON ONE ACCOUNT</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row text-center">
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="new_games_item">
+						<img src="img/b_map1.png" alt="">
+						<div class="upcoming_title">
+							<h3><a href="games.html">Stock</a></h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="new_games_item">
+						<img src="img/b_map2.png" alt="">
+						<div class="upcoming_title">
+							<h3><a href="games.html">Futures</a></h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="new_games_item">
+						<img src="img/b_map3.png" alt="">
+						<div class="upcoming_title">
+							<h3><a href="games.html">Forex</a></h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="new_games_item">
+						<img src="img/b_map4.png" alt="">
+						<div class="upcoming_title">
+							<h3><a href="games.html">Options</a></h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Upcoming Games Area =================-->
+
+
+
+
+
+
+
+	<!--========== Start Testimonials Area ==================-->
+	<section class="testimonials_area section_gap">
+		<div class="container">
+			<div class="testi_slider owl-carousel">
+				<div class="testi_item">
+					<img src="img/quote.png" alt="">
+					<h4>OPEN ACCOUNT AND TRADE
+ON ANY DEVICE</h4>
+					<ul class="list">
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+					</ul>
+					<div class="wow fadeIn" data-wow-duration="1s">
+						<p>
+            MetaTrader 5 for Windows
+						</p>
+					</div>
+				</div>
+				<div class="testi_item">
+					<img src="img/quote.png" alt="">
+					<h4>OPEN ACCOUNT AND TRADE
+ON ANY DEVICE</h4>
+					<ul class="list">
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+					</ul>
+					<div class="wow fadeIn" data-wow-duration="1s">
+						<p>
+            MetaTrader 5 for iPhone and iPad
+						</p>
+					</div>
+				</div>
+				<div class="testi_item">
+					<img src="img/quote.png" alt="">
+					<h4>OPEN ACCOUNT AND TRADE
+ON ANY DEVICE</h4>
+					<ul class="list">
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+						<li><a href="#"><i class="fa fa-star"></i></a></li>
+					</ul>
+					<div class="wow fadeIn" data-wow-duration="1s">
+						<p>
+            MetaTrader 5 for Android
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Testimonials Area ================-->
+
+
+	
+
+
+	<!--================ Start Frequently Asked Questions Area ================-->
+	<section class="frequently_area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+          <div class="main_title">
+            <br><br>
+						<h2>MT5 global</h2>
+						<h1>MT5 global</h1>
+					</div>
+				</div>
+			</div>
+      MT5 is a powerful tool for a trader to work on any financial markets (stocks, futures, bonds, forex) within an MT5 Global account, providing unlimited opportunities for technical analysis and trading automation (trading robots and advisors).      <br><br>
+		</div>
+	</section>
+	<!--================ End Frequently Asked Questions Area ================-->
+
+	
+
+	<!--================ Start Newsletter Area ================-->
+	<section class="newsletter_area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="newsletter_inner">
+						<h1>Subscribe Our Newsletter</h1>
+						<p>We won’t send any kind of spam</p>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<aside class="newsletter_widget">
+						<div id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get" class="subscribe_form relative">
+								<div class="input-group d-flex flex-row">
+									<input name="EMAIL" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'"
+									 required="" type="email">
+									<button class="btn primary_btn">Subscribe</button>
+								</div>
+							</form>
+						</div>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Newsletter Area ================-->
+
+	<!--================Footer Area =================-->
+	<footer class="footer_area section_gap_top">
+		<div class="container">
+			<div class="row footer_inner">
+				<div class="col-lg-3 col-sm-6">
+					<aside class="f_widget ab_widget">
+					
+					</aside>
+				</div>
+
+			</div>
+			<div class="row single-footer-widget">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="copy_right_text">
+						<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | J2T <i class="fa fa-heart-o" aria-hidden="true"></i>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</footer>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="vendors/isotope/isotope-min.js"></script>
+	<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+	<script src="vendors/counter-up/jquery.counterup.min.js"></script>
+	<script src="js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="js/gmaps.min.js"></script>
+	<script src="js/theme.js"></script>
+</body>
+
+</html>
